96bb99d86184ba2be188bac9487f30ff29421afd
lee <why48382@gmail.com>
2025-10-13 16:29:04 +0900

    feat: inbound repository 구현

---
+++ src/main/java/com/fourweekdays/fourweekdays/Inbound/repository/InboundRepository.java
@@ -0,0 +1,8 @@
+package com.fourweekdays.fourweekdays.Inbound.repository;
+
+import com.fourweekdays.fourweekdays.Inbound.model.entity.Inbound;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InboundRepository extends JpaRepository<Inbound, Long> {
+
+}
