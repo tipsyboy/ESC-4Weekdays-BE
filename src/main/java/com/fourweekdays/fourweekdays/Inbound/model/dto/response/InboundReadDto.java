024712f1a24b7acaaebfb9fc77da6a90c6b2b339
lee <why48382@gmail.com>
2025-10-13 16:28:51 +0900

    feat: inbound crud dto 구현

---
+++ src/main/java/com/fourweekdays/fourweekdays/Inbound/model/dto/response/InboundReadDto.java
@@ -0,0 +1,31 @@
+package com.fourweekdays.fourweekdays.Inbound.model.dto.response;
+
+import com.fourweekdays.fourweekdays.Inbound.model.entity.Inbound;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class InboundReadDto {
+    private Long inboundId;
+    private Long memberId;
+    private Long purchaseOrderId;
+    private Long quantity;
+    private String inboundStatus;
+    private PurchaseOrderDetailDto purchaseOrderDetailDto;
+    private ProductDetailDto productDetailDto;
+
+    public static InboundReadDto from(Inbound entity) {
+        // 이거를 도메인이 하게 만들겠다는 거잖아
+        PurchaseOrderDetailDto purchaseOrderDto = PurchaseOrderDetailDto.from(entity.getPurchaseOrder());
+        ProductDetailDto productDto = ProductDetailDto.from(entity.getPurchaseOrder().getProduct());
+        return InboundReadDto.builder()
+                .inboundId(entity.getId())
+                .memberId(entity.getMember().getId())
+                .quantity(entity.getQuantity())
+                .inboundStatus(entity.getStatus())
+                .purchaseOrderDetailDto(purchaseOrderDto)
+                .productDetailDto(productDto)
+                .build();
+    }
+}
