30facdccc2b1c7f32850bfaf02b0dcea7a136a4c
lee <why48382@gmail.com>
2025-10-14 15:02:56 +0900

    fix: VendorController RequestMapping 수정
    /api 추가

--- src/main/java/com/fourweekdays/fourweekdays/vendor/controller/VendorController.java
+++ src/main/java/com/fourweekdays/fourweekdays/vendor/controller/VendorController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vendor")
+@RequestMapping("/api/vendor")
 @RequiredArgsConstructor
 public class VendorController {
     private final VendorService vendorService;
