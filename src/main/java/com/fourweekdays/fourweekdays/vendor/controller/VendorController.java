26767974e0411b603e35422bea87d6d7e3321488
lee <why48382@gmail.com>
2025-10-14 10:48:15 +0900

    feat: vendor 등록기능 구현
    vendor 엔티티에 getter builder 추가
    
    Close #41

---
+++ src/main/java/com/fourweekdays/fourweekdays/vendor/controller/VendorController.java
@@ -0,0 +1,53 @@
+package com.fourweekdays.fourweekdays.vendor.controller;
+
+import com.fourweekdays.fourweekdays.common.BaseResponse;
+import com.fourweekdays.fourweekdays.vendor.model.dto.request.VendorCreateDto;
+import com.fourweekdays.fourweekdays.vendor.model.dto.request.VendorUpdateDto;
+import com.fourweekdays.fourweekdays.vendor.model.dto.response.VendorReadDto;
+import com.fourweekdays.fourweekdays.vendor.model.entity.Vendor;
+import com.fourweekdays.fourweekdays.vendor.service.VendorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vendor")
+@RequiredArgsConstructor
+public class VendorController {
+    private final VendorService vendorService;
+
+    @GetMapping
+    public ResponseEntity<BaseResponse<Long>> createVendor(VendorCreateDto dto) {
+        Long result = vendorService.create(dto);
+        return ResponseEntity.ok(BaseResponse.success(result));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BaseResponse<VendorReadDto>> readVendor(@PathVariable Long id) {
+        VendorReadDto result = vendorService.read(id);
+        return ResponseEntity.ok(BaseResponse.success(result));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<BaseResponse<List<VendorReadDto>>> readAllVendors(Integer page, Integer size) {
+        List<VendorReadDto> result = vendorService.readAll(page, size);
+        return ResponseEntity.ok(BaseResponse.success(result));
+    }
+
+    @GetMapping("/update")
+    public ResponseEntity<BaseResponse<Long>> updateVendor(VendorUpdateDto dto) {
+        Long result = vendorService.update(dto);
+        return ResponseEntity.ok(BaseResponse.success(result));
+    }
+
+    @GetMapping("/delete")
+    public ResponseEntity<BaseResponse<String>> deleteVendor(Long id) {
+        vendorService.delete(id);
+        return ResponseEntity.ok(BaseResponse.success("Deleted"));
+    }
+}
--- src/main/java/com/fourweekdays/fourweekdays/vendor/repository/VendorRepository.java
+++ src/main/java/com/fourweekdays/fourweekdays/vendor/repository/VendorRepository.java
@@ -1,5 +1,6 @@
-package com.fourweekdays.fourweekdays.vendor;
+package com.fourweekdays.fourweekdays.vendor.repository;
 
+import com.fourweekdays.fourweekdays.vendor.model.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
