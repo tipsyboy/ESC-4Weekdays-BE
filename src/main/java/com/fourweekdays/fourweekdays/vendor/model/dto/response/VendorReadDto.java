58e278734414fc12cef15219832b7af5bb55d22d
lee <why48382@gmail.com>
2025-10-14 10:48:51 +0900

    feat: vendor 등록기능 구현
    vendor 엔티티에 getter builder 추가
    
    Close #42, #43

---
+++ src/main/java/com/fourweekdays/fourweekdays/vendor/model/dto/response/VendorReadDto.java
@@ -0,0 +1,29 @@
+package com.fourweekdays.fourweekdays.vendor.model.dto.response;
+
+import com.fourweekdays.fourweekdays.common.Address;
+import com.fourweekdays.fourweekdays.vendor.model.entity.Vendor;
+import jakarta.persistence.Embedded;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class VendorReadDto {
+    private Long id;
+    private String businessRegistrationNo; // 사업자 등록 번호
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    public static VendorReadDto from(Vendor entity) {
+        return  VendorReadDto.builder()
+                .id(entity.getId())
+                .businessRegistrationNo(entity.getBusinessRegistrationNo())
+                .name(entity.getName())
+                .phoneNumber(entity.getPhoneNumber())
+                .email(entity.getEmail())
+//                .address(entity.getAddress())
+                .build();
+    }
+}
