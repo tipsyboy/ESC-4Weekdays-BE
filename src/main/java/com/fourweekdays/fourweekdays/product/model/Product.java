528d883ba022760d3a2d2739aa7d194b3a2fa900
tipsyboy <gudah13@naver.com>
2025-10-15 10:32:53 +0900

    feat: 공급업체, 상품 엔티티를 재정의하고 연관관계를 설정한다. ---
    refactor: 상품 엔티티를 재설계
    refactor: 상품 상태를 재설계
    refactor: 공급 업체 엔티티를 재설계
    refactor: 공급 업체 상태를 재설계
    feat: 상품-공급 업체 엔티티 연관관계 매핑

--- src/main/java/com/fourweekdays/fourweekdays/product/model/Product.java
+++ src/main/java/com/fourweekdays/fourweekdays/product/model/Product.java
@@ -1,6 +1,7 @@
 package com.fourweekdays.fourweekdays.product.model;
 
 import com.fourweekdays.fourweekdays.common.BaseEntity;
+import com.fourweekdays.fourweekdays.vendor.model.entity.Vendor;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,6 +14,7 @@ public class Product extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
     private Long id;
 
     @Column(nullable = false, length = 200)
@@ -21,12 +23,20 @@ public class Product extends BaseEntity {
     @Column(nullable = false, unique = true, length = 50)
     private String productCode;
 
+    @Column(length = 50)
+    private String unit; // 단위 (예: EA, Box ...)
+
     private Long unitPrice; // 단가
 
     @Column(length = 1000)
     private String description;
 
-    private ProductStatus status;
+    @Enumerated(EnumType.STRING)
+    private ProductStatus status; //ACTIVE, INACTIVE, DISCONTINUED
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vendor_id")
+    private Vendor vendor; // 공급업체
 
 //    @Column(nullable = false, unique = true, length = 50)
 //    private String barcode; // 바코드 추후 구현
--- src/main/java/com/fourweekdays/fourweekdays/product/model/ProductStatus.java
+++ src/main/java/com/fourweekdays/fourweekdays/product/model/ProductStatus.java
@@ -2,6 +2,6 @@ package com.fourweekdays.fourweekdays.product.model;
 
 public enum ProductStatus {
     ACTIVE,
-    INACTIVE, // 재고 부족 등으로 일시적 0
+    INACTIVE, // 품절
     DISCONTINUED // 단종
 }
