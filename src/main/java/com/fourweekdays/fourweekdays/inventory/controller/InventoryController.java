586d652ddc2f722f341c53b2bf0a9986d79cd6b5
lee <why48382@gmail.com>
2025-10-16 11:26:03 +0900

    feat: swagger를 위한 controller를 생성
    재고 목록 조회, 상세 조회 api 제작

---
+++ src/main/java/com/fourweekdays/fourweekdays/inventory/controller/InventoryController.java
@@ -0,0 +1,28 @@
+package com.fourweekdays.fourweekdays.inventory.controller;
+
+import com.fourweekdays.fourweekdays.common.BaseResponse;
+import com.fourweekdays.fourweekdays.inventory.model.dto.request.InventoryFilterDto;
+import com.fourweekdays.fourweekdays.inventory.model.dto.response.InventoryListDto;
+import com.fourweekdays.fourweekdays.inventory.model.dto.response.InventoryReadDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventories") // 복수형으로 작성
+public class InventoryController {
+
+    // 재고 목록 조회
+    @GetMapping
+    public ResponseEntity<BaseResponse<InventoryListDto>> inventoryList(InventoryFilterDto dto) {
+        return ResponseEntity.ok(BaseResponse.success(new InventoryListDto()));
+    }
+
+    // 재고 상세 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<BaseResponse<InventoryReadDto>> inventoryRead(@PathVariable Integer id) {
+        return ResponseEntity.ok(BaseResponse.success(new InventoryReadDto()));
+    }
+}
