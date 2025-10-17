c000f3f40cb35856182790a450b983730c117b11
lee <why48382@gmail.com>
2025-10-17 12:30:53 +0900

    fix: 재고 검색 dto 오타 수정

--- src/main/java/com/fourweekdays/fourweekdays/inventory/model/dto/request/InventorySearchDto.java
+++ src/main/java/com/fourweekdays/fourweekdays/inventory/model/dto/request/InventorySearchDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class InventorySearchDto {
     //    private Product product; // 상품 상세 조건들
-    private String productId; // 재고 번호
+    private String inventoryId;; // 재고 번호
     private String location; // 보관 위치
     private int quantity; // 수량
     private String memberName; // 작업자
