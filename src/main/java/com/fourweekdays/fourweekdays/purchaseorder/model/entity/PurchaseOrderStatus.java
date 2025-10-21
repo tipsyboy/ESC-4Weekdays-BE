a8e978c30d8ad341461152d8721ffbb23c12a1f5
tipsyboy <gudah13@naver.com>
2025-10-21 22:29:19 +0900

    refactor: 발주 상태 enum을 수정한다.

--- src/main/java/com/fourweekdays/fourweekdays/purchaseorder/model/entity/PurchaseOrderStatus.java
+++ src/main/java/com/fourweekdays/fourweekdays/purchaseorder/model/entity/PurchaseOrderStatus.java
@@ -4,12 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum PurchaseOrderStatus {
-//    DRAFT("임시저장"), // 작성중
+
     REQUESTED("발주 요청"), // 승인 대기
-    APPROVED("승인 완료"), // 승인됨
-//    ORDERED("발주완료"), // 공급업체에 발주함
-//    PARTIALLY_RECEIVED("부분입고"),
-    COMPLETED("완료"), // 입고 완료
+    APPROVED("승인 완료"), // 승인됨 (공급사로 발주 전송)
+    AWAITING_DELIVERY("납품 대기"), // 공급사 납품(ASN) 대기
+    DELIVERED("도착 예정"), // ASN 수신 완료 → Inbound 생성 완료
     CANCELLED("취소"); // 취소됨
 
     private final String status;
