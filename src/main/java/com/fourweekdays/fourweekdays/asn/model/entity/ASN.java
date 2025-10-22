1e9933479bcc834f425e100dba8913fe1c308409
tipsyboy <gudah13@naver.com>
2025-10-23 00:30:33 +0900

    feat: ASN 엔티티를 구현한다. --- feat: ASN 엔티티 구현 refactor: Vendor에 api key를 추가하고 발급 메서드 추가

---
+++ src/main/java/com/fourweekdays/fourweekdays/asn/model/entity/ASN.java
@@ -0,0 +1,44 @@
+package com.fourweekdays.fourweekdays.asn.model.entity;
+
+import com.fourweekdays.fourweekdays.common.BaseEntity;
+import com.fourweekdays.fourweekdays.purchaseorder.model.entity.PurchaseOrder;
+import com.fourweekdays.fourweekdays.vendor.model.entity.Vendor;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ASN extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String asnCode; // ASN 고유번호
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vendor_id", nullable = false)
+    private Vendor vendor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchase_order_id")
+    private PurchaseOrder purchaseOrder;
+
+    private LocalDateTime expectedDate; // 도착 날짜
+
+
+    public static ASN create(Vendor vendor, PurchaseOrder purchaseOrder, String asnCode, LocalDateTime expectedDate) {
+        ASN asn = new ASN();
+        asn.vendor = vendor;
+        asn.purchaseOrder = purchaseOrder;
+        asn.asnCode = asnCode;
+        asn.expectedDate = expectedDate;
+        return asn;
+    }
+
+}
