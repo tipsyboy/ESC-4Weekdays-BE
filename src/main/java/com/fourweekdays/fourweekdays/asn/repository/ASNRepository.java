729f56ce04c014319f02d6f6e42e880e415bc2d5
tipsyboy <gudah13@naver.com>
2025-10-23 01:02:13 +0900

    feat: ASN 검증 필터를 구현한다.

---
+++ src/main/java/com/fourweekdays/fourweekdays/asn/repository/ASNRepository.java
@@ -0,0 +1,10 @@
+package com.fourweekdays.fourweekdays.asn.repository;
+
+import com.fourweekdays.fourweekdays.asn.model.entity.ASN;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ASNRepository extends JpaRepository<ASN, Long> {
+
+    boolean existsASNSByAsnCode(String asnCode);
+}
+
