d5b72d80ebf8532f1602c35d1f5794be990a12f4
tipsyboy <gudah13@naver.com>
2025-10-23 14:47:36 +0900

    feat: ASN 페이징 조회를 구현한다.

--- src/main/java/com/fourweekdays/fourweekdays/asn/repository/ASNRepository.java
+++ src/main/java/com/fourweekdays/fourweekdays/asn/repository/ASNRepository.java
@@ -3,9 +3,6 @@ package com.fourweekdays.fourweekdays.asn.repository;
 import com.fourweekdays.fourweekdays.asn.model.entity.ASN;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ASNRepository extends JpaRepository<ASN, Long>, ASNRepositoryCustom {
-}
 }
 
