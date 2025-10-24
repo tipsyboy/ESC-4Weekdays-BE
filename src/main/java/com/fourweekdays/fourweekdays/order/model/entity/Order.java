3a2a246caa16e68051ca3e1641da5e6433df6403
lee <why48382@gmail.com>
2025-10-24 15:27:57 +0900

    refactor: order패키지 구조 변경
    1. 컨벤션에 맞춰 패키지 생성하여 Order, OrderStatus 위치 변경
    2. FranchiseStore에 apiKey 컬럼 생성

--- src/main/java/com/fourweekdays/fourweekdays/order/model/entity/Order.java
+++ src/main/java/com/fourweekdays/fourweekdays/order/model/entity/Order.java
@@ -1,7 +1,8 @@
-package com.fourweekdays.fourweekdays.order;
+package com.fourweekdays.fourweekdays.order.model.entity;
 
 import com.fourweekdays.fourweekdays.common.BaseEntity;
 import com.fourweekdays.fourweekdays.franchise.model.entity.FranchiseStore;
+import com.fourweekdays.fourweekdays.order.OrderProductItem;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
