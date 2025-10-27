cad32c884c9dbafb804a5dc146f92a029d88696d
wonzzu <wonzzu@naver.com>
2025-10-27 15:08:24 +0900

    refator:member 엔티티 update 로직 변경 & service update 로직 변경

--- src/main/java/com/fourweekdays/fourweekdays/member/model/dto/MemberUpdateDto.java
+++ src/main/java/com/fourweekdays/fourweekdays/member/model/dto/MemberUpdateDto.java
@@ -2,6 +2,9 @@ package com.fourweekdays.fourweekdays.member.model.dto;
 
 import com.fourweekdays.fourweekdays.member.model.entity.AuthStatus;
 import com.fourweekdays.fourweekdays.member.model.entity.MemberRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +16,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberUpdateDto {
 
+    @NotBlank(message = "이름을 입력해주세요")
     private String name;
-    private String phoneNumber;
-    private MemberRole role;
-    private AuthStatus status;
+
+    @NotBlank(message = "비밀번호을 입력해 주세요")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).{8,}$", message = "비밀번호는 영문과 숫자를 포함하여 8자 이상이어야 합니다.")
     private String password;
+
+    @NotBlank(message = "핸드폰번호를 입력해 주세요")
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "핸드폰번호 형식이 올바르지 않습니다.")
+    private String phoneNumber;
+
+    @NotNull(message = "권한을 선택해주세요")
+    private MemberRole role;
+
+    @NotNull(message = "상태를 선택해주세요")
+    private AuthStatus status;
 
 }
