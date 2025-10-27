16791cbd12da58ace13c60d01112ba1ef9275b0e
lee <why48382@gmail.com>
2025-10-27 17:16:51 +0900

    feat: Order조회 기능 구현
    ㄹㅇ 조회임 ㅇㅇ

---
+++ src/main/java/com/fourweekdays/fourweekdays/order/model/dto/response/OrderReadDto.java
@@ -0,0 +1,32 @@
+package com.fourweekdays.fourweekdays.order.model.dto.response;
+
+import com.fourweekdays.fourweekdays.order.model.entity.Order;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class OrderReadDto {
+    private Long id;
+    private String orderCode;
+    private String franchiseName;
+    private LocalDateTime dueDate;
+    private String description;
+
+    private List<OrderProductResponseDto> products;
+
+    public static OrderReadDto from(Order entity) {
+        return OrderReadDto.builder()
+                .id(entity.getOrderId())
+                .orderCode(entity.getOrderCode())
+                .dueDate(entity.getDueDate())
+                .description(entity.getDescription())
+                .products(entity.getItems().stream()
+                        .map(OrderProductResponseDto::from)
+                        .toList())
+                .build();
+    }
+}
