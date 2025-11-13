b1f9d5d1885af812cf3828a59b3877a71ea611bb
lee <why48382@gmail.com>
2025-11-13 11:23:35 +0900

    refactor: outbound member를 연결

--- src/main/java/com/fourweekdays/fourweekdays/outbound/model/dto/request/OutboundCreateDto.java
+++ src/main/java/com/fourweekdays/fourweekdays/outbound/model/dto/request/OutboundCreateDto.java
@@ -1,22 +1,18 @@
 package com.fourweekdays.fourweekdays.outbound.model.dto.request;
 
-import com.fourweekdays.fourweekdays.inbound.model.dto.request.InboundProductDto;
 import com.fourweekdays.fourweekdays.member.model.entity.Member;
 import com.fourweekdays.fourweekdays.order.model.entity.Order;
 import com.fourweekdays.fourweekdays.outbound.model.entity.Outbound;
 import com.fourweekdays.fourweekdays.outbound.model.entity.OutboundStatus;
-import com.fourweekdays.fourweekdays.outbound.model.entity.OutboundType;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Builder
 public class OutboundCreateDto {
 
-    private Long memberId;
     private Long orderId;
     private LocalDateTime scheduledDate; // 출고 예상 시간
     private String description;
