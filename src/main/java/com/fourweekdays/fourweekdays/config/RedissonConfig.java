65a0e6372b6d9b10155a810fd4bfa2064216ee66
tipsyboy <gudah13@naver.com>
2025-11-17 15:48:31 +0900

    release server with redisson

--- src/main/java/com/fourweekdays/fourweekdays/config/RedissonConfig.java
+++ src/main/java/com/fourweekdays/fourweekdays/config/RedissonConfig.java
@@ -3,18 +3,28 @@ package com.fourweekdays.fourweekdays.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://127.0.0.1:6379")
-                .setPassword("myStrongPassword");
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setPassword(redisPassword);
         return Redisson.create(config);
     }
 }
