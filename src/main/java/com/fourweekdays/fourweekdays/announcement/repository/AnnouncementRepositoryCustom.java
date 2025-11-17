6d742f5cd49ac2ef1f8aa04c3fef69e344457bd1
wonzzu <wonzzu@naver.com>
2025-11-17 10:00:06 +0900

    feat : 공지 검색 추가

--- src/main/java/com/fourweekdays/fourweekdays/announcement/repository/AnnouncementRepositoryCustom.java
+++ src/main/java/com/fourweekdays/fourweekdays/announcement/repository/AnnouncementRepositoryCustom.java
@@ -1,10 +1,13 @@
 package com.fourweekdays.fourweekdays.announcement.repository;
 
+import com.fourweekdays.fourweekdays.announcement.model.dto.request.AnnouncementSearchDto;
 import com.fourweekdays.fourweekdays.announcement.model.entity.Announcement;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface AnnouncementRepositoryCustom {
     Page<Announcement> findAllWithPaging(Pageable pageable);
+
+    Page<Announcement> searchAnnouncement(AnnouncementSearchDto announcementSearchDto, Pageable pageable);
 }
 
