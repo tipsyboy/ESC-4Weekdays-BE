e3d63d993d9701abd2995e83bebcaf11b6c3a10e
LEEPC <why48382@gmail.com>
2025-11-21 00:48:23 +0900

    refactor: Outbound 조회시 상품을 반환하게 변경

--- src/main/java/com/fourweekdays/fourweekdays/outbound/repository/OutboundRepositoryCustomImpl.java
+++ src/main/java/com/fourweekdays/fourweekdays/outbound/repository/OutboundRepositoryCustomImpl.java
@@ -11,6 +11,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.fourweekdays.fourweekdays.inbound.model.entity.QInbound.inbound;
+import static com.fourweekdays.fourweekdays.inbound.model.entity.QInboundProduct.inboundProduct;
+import static com.fourweekdays.fourweekdays.outbound.model.entity.QOutboundProductItem.outboundProductItem;
+import static com.fourweekdays.fourweekdays.product.model.entity.QProduct.product;
+import static com.fourweekdays.fourweekdays.purchaseorder.model.entity.QPurchaseOrder.purchaseOrder;
+import static com.fourweekdays.fourweekdays.vendor.model.entity.QVendor.vendor;
+
 @Repository
 @RequiredArgsConstructor
 public class OutboundRepositoryCustomImpl implements OutboundRepositoryCustom {
@@ -22,14 +29,19 @@ public class OutboundRepositoryCustomImpl implements OutboundRepositoryCustom {
     public Page<Outbound> findAllWithPaging(Pageable pageable) {
         List<Outbound> outbounds = queryFactory
                 .selectFrom(outbound)
+                .leftJoin(outbound.items, outboundProductItem).fetchJoin()             // OutboundProductItem
+                .leftJoin(outboundProductItem.product, product).fetchJoin()     // Product
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .orderBy(outbound.createdAt.desc())
+                .distinct()                                       // 중복 제거 필수
                 .fetch();
+
         Long total = queryFactory
                 .select(outbound.count())
                 .from(outbound)
                 .fetchOne();
+
         return new PageImpl<>(outbounds, pageable, total);
     }
 }
