9ef78edc913baafede5d27e0c11b84c624d10832
tipsyboy <gudah13@naver.com>
2026-01-21 00:24:46 +0900

    refactor: inventory의 es 관련 로직을 제거한다.

--- src/main/java/com/fourweekdays/fourweekdays/inventory/controller/InventoryController.java
+++ src/main/java/com/fourweekdays/fourweekdays/inventory/controller/InventoryController.java
@@ -3,7 +3,6 @@ package com.fourweekdays.fourweekdays.inventory.controller;
 import com.fourweekdays.fourweekdays.common.BaseResponse;
 import com.fourweekdays.fourweekdays.inventory.model.dto.request.InventorySearchRequest;
 import com.fourweekdays.fourweekdays.inventory.model.dto.response.ProductInventoryResponse;
-import com.fourweekdays.fourweekdays.inventory.service.InventorySearchService;
 import com.fourweekdays.fourweekdays.inventory.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,20 +15,12 @@ import org.springframework.web.bind.annotation.*;
 public class InventoryController {
 
     private final InventoryService inventoryService;
-    private final InventorySearchService inventorySearchService;
 
     @PostMapping("/search")
     public ResponseEntity<BaseResponse<Page<ProductInventoryResponse>>> inventoriesByProduct(@RequestParam(defaultValue = "0") int page,
                                                                                              @RequestParam(defaultValue = "10") int size,
                                                                                              @RequestBody InventorySearchRequest request) {
         return ResponseEntity.ok(BaseResponse.success(inventoryService.searchInventoryByProduct(request, page, size)));
-    }
-
-    @PostMapping("/search/es")
-    public ResponseEntity<BaseResponse<Page<ProductInventoryResponse>>> searchInventories(@RequestParam(defaultValue = "0") int page,
-                                                                                          @RequestParam(defaultValue = "10") int size,
-                                                                                          @RequestBody InventorySearchRequest searchRequest) {
-        return ResponseEntity.ok(BaseResponse.success(inventorySearchService.searchInventoryByProduct(searchRequest, page, size)));
     }
 
     // 재고 상세 조회
